11da4564a7901f8dc57d8fdc1cd2a77f3b949992
Anton Ilin <anton@ilin.dn.ua>
2014-07-20 16:39:30 +0300

    Fix deprecation warning

--- spec/url_template_spec.rb
+++ spec/url_template_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'familysearch/client'
 
 describe FamilySearch::URLTemplate do
-  
+
   describe "initializing" do
     def template_hash
       {
@@ -13,15 +13,15 @@ describe FamilySearch::URLTemplate do
         "title" => "Person Change Summary"
       }
     end
-    
+
     def client
       FamilySearch::Client.new
     end
-    
+
     it "should accept the client object and a hash" do
       template = FamilySearch::URLTemplate.new(client,template_hash)
     end
-    
+
     it "should set instance attributes for the objects in the hash" do
       template = FamilySearch::URLTemplate.new(client,template_hash)
       template.template.should == "https://sandbox.familysearch.org/platform/tree/persons/{pid}/change-summary{?access_token}"
@@ -31,7 +31,7 @@ describe FamilySearch::URLTemplate do
       template.title.should == 'Person Change Summary'
     end
   end
-  
+
   describe "get" do
     def template_hash
       {
@@ -39,10 +39,10 @@ describe FamilySearch::URLTemplate do
         "type" => "application/json,application/x-fs-v1+json,application/x-fs-v1+xml,application/xml,text/html",
         "accept" => "*/*",
         "allow" => "GET",
-        "title" => "Person With Relationships" 
+        "title" => "Person With Relationships"
       }
     end
-    
+
     def person_template_hash
       {
         "template" => "https://sandbox.familysearch.org/platform/tree/persons/{pid}{?access_token}",
@@ -52,7 +52,7 @@ describe FamilySearch::URLTemplate do
         "title" => "Person"
       }
     end
-    
+
     def client()
       unless @client
         @client = FamilySearch::Client.new(:key => 'WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK' )
@@ -61,48 +61,48 @@ describe FamilySearch::URLTemplate do
       end
       @client
     end
-    
+
     def template
       @template ||= FamilySearch::URLTemplate.new client, template_hash
     end
-    
+
     def person_template
       @person_template ||= FamilySearch::URLTemplate.new client, person_template_hash
     end
-    
+
     it "should accept a hash of options" do
       VCR.use_cassette('person_with_relationship') do
         template.get 'person' => 'KWQX-52J'
       end
     end
-    
+
     it "should raise an error if the get isn't in the allowed list" do
       VCR.use_cassette('person_with_relationship') do
-        template.stub!(:allow).and_return([])
+        template.stub(:allow).and_return([])
         expect {template.get 'person' => 'KWQX-52J'}.to raise_error(FamilySearch::Error::MethodNotAllowed)
       end
     end
-    
+
     it "should raise an error if a template value isn't found in the template" do
       VCR.use_cassette('person_with_relationship') do
         expect {template.get 'person_id' => 'KWQX-52J'}.to raise_error(FamilySearch::Error::TemplateValueNotFound)
       end
     end
-    
+
     it "should call get on the client with the url and values on the querystring" do
       VCR.use_cassette('person_with_relationship') do
         client.should_receive(:get).with('https://sandbox.familysearch.org/platform/tree/persons-with-relationships?person=KWQX-52J').and_call_original
         template.get 'person' => 'KWQX-52J'
       end
     end
-    
+
     it "should call get on the client with the url and no querystring if nothing needs to be called" do
       VCR.use_cassette('person_by_id') do
         client.should_receive(:get).with('https://sandbox.familysearch.org/platform/tree/persons/KWQX-52J')
         person_template.get 'pid' => 'KWQX-52J'
       end
     end
-    
+
     it "should accept symbols in the values hash" do
       VCR.use_cassette('person_with_relationship') do
         client.should_receive(:get).with('https://sandbox.familysearch.org/platform/tree/persons-with-relationships?person=KWQX-52J').and_call_original
